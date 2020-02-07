e3064a980c7a28113f99bb640919d702bccc7a1e
zkxshg <35519607+zkxshg@users.noreply.github.com>
2020-02-07 15:49:10 +0800

    Update Simple_classes.cpp

--- Simple_classes.cpp
+++ Simple_classes.cpp
@@ -72,7 +72,7 @@ int demo3_6()
     cout << " 123 / 5 " << 123 / 5 << endl;
     cout << " 1.23e5 / 7.0 " << 1.23e5 / 7.0 << endl;
     cout << " 1.23e5f / 7.0f " << 1.23e5f / 7.0f << endl;
-    // mod
+    // mod 
     const int L_p_s = 14;
     int lbs;
     cout << "Your weight in pounds: ";
@@ -93,7 +93,7 @@ int demo3_5()
     cout << "tub : " << tub << ", a millon tub : " << million * tub;
     cout << ", \n and ten million tubs = " << 10 * million * tub << endl;
     cout << "mint : " << mint << " and a million mint = " << million * mint << endl;
-    // float overloading
+    // float overloading 浮点数过载
     float a = 2.35E+22f;
     float b = a + 1.0f;
     cout << "a = " << a << endl;
@@ -103,7 +103,7 @@ int demo3_5()
 
 int demo3_4()
 {
-    // char / int = char , \a , \b , \n , \".
+    // char, int = char , \a , \b , \n , \".
     char ch;
     cout << "Enter a char : " << endl;
     cin >> ch;
@@ -115,6 +115,7 @@ int demo3_4()
     cout << "The char+1 is : " << ch << endl;
     cout << "The ascii of ch+1 is : " << i << endl;
     cout << "The put of ch+1 is : ";
+    // cout.put()
     cout.put(ch);
     cout << endl;
     // \a : alarm ; \b : back.
@@ -130,11 +131,10 @@ int demo3_4()
 int demo3_3()
 {
     // 42 / 042 / 0x42
-    // 10 / hex / oct
     cout << "42: " << 42 << endl;
     cout << "042: " << 042 << endl;
     cout << "0x42: " << 0x42 << endl;
-
+    // 10 / hex / oct
     cout << "42:" << 42 << endl;
     cout << hex;
     cout << "hex 42:" << 42 << endl;
@@ -145,7 +145,7 @@ int demo3_3()
 
 int demo3_2()
 {
-    // unsigned short
+    // unsigned short 非负类型
     short sam = SHRT_MAX;
     unsigned short sue = sam;
     cout << "Sam : " << sam << " and sue : " << sue <<endl;
@@ -164,26 +164,22 @@ int demo3_2()
 int demo3_1()
 {
     // int / short / long / long long
-    // sizeof (int); sizeof n_short;
     // climits: SHRT_MAX / SHRT_MIN / CHAR_BIT
-
     int n_int = INT_MAX;
     short n_short = SHRT_MAX;
     long n_long = LONG_MAX;
     long long n_llong  = LLONG_MAX;
-
+    // sizeof (int); sizeof n_short;
     cout << "int is" << sizeof (int) << "bytes." <<endl;
     cout << "short is" << sizeof n_short << "bytes." <<endl;
     cout << "long is" << sizeof n_llong << "bytes." <<endl;
     cout << " long long is" << sizeof n_llong << "bytes." <<endl;
     cout << endl;
-
     cout << "Max of: " << endl;
     cout << "int is " << n_int <<endl;
     cout << "short is" << n_short <<endl;
     cout << "long is" << n_long  <<endl;
     cout << "llong is" << n_llong  <<endl<<endl;
-
     cout << "Min int is" << INT_MIN <<endl;
     cout << "Bit per byte = " << CHAR_BIT <<endl;
     cin.get();
